ca7f0d3772b5303c5f1fb5f921324560bdb4f7fa
s4mp41xao <samribe@outloook.com>
2023-04-26 03:33:34 -0300

    Offers component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx'
+import Offers from './components/Offers.jsx';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Offers />
     </div>
   );
 }
